12f87a2ae2e2af11656672b9756312ab5de6fb2e
SammyHerring <sammy.herring@me.com>
2017-03-18 19:03:40 +0000

    v0.4.1 r3a
    Unit Test for v0.4.1 regarding the ‘System Setup’ Unit, denoted
    as v0.4.1 r3a.

---
+++ 3 - Unit Testing/3. System Setup Unit.ino
@@ -0,0 +1,23 @@
+/* ======================= SYSTEM SETUP ======================== */
+
+STARTUP(
+  WiFi.selectAntenna(ANT_EXT); //Selects the external u.Fl Anetenna for the WiFi connection.
+  System.enableUpdates(); //Allows the device to download OTA updates at boot.
+  );
+SYSTEM_MODE(MANUAL); //Forces the system to wait for cloud processing until allowed by algorithm.
+SYSTEM_THREAD(ENABLED); //Allows a secondary thread to be used for the cloud processing.
+
+//The setup and loop components added below to allow the file to successfully compile.
+//However, this unit does not run within them as they are apart of the system setup.
+
+/* ================ SETUP CODE (RUNS ONCE) ==================== */
+
+void setup() {
+
+}
+
+/* ================ MAIN CODE (RUN REPEATEDLY) ================= */
+
+void loop() {
+
+}
