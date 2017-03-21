e4fc6b32252e933a19fa02a86db04384bd41fa83
SammyHerring <sammy.herring@social-systems.co.uk>
2017-03-21 12:31:04 +0000

    v0.4.1 r5a
    Unit Test for v0.4.1 regarding the ‘Main System Loop’ Unit, denoted
    as v0.4.1 r5a.

---
+++ 3 - Unit Testing/5. Main System Loop Unit.ino
@@ -0,0 +1,44 @@
+//Any earlier values are required for optimal operation, but may be discounted from this test.
+
+/* ================ SETUP CODE (RUNS ONCE) ==================== */
+
+void setup() {
+
+}
+
+/* ================ MAIN CODE (RUN REPEATEDLY) ================= */
+
+void loop() {
+  updateTime(); //Function not considered in this test, but required for logic operation.
+  //Continue if connected to Particle Cloud
+  while (Particle.connected()) {
+    delay(2000);
+    updateTime(); //Function not considered in this test, but required for logic operation.
+
+    //Print time values on serial
+    Serial.println(formattedTime);
+    Serial.println(formattedDay);
+    Serial.println(period);
+
+    delay(5000);
+    updateTime(); //Function not considered in this test, but required for logic operation.
+    delay(5000);
+
+    //Checks reservation status and sends appropriate notification.
+    //See Logic tables on page 41 for more information.
+    if (reservation[day-1][period] == 0) {
+      if (pirState == HIGH) {
+        //Notification Module not implemented, therefore tested using Serial output.
+        Serial.println("Vacant. No movement detected.");
+      } else {
+        //Notification Module not implemented, therefore tested using Serial output.
+        Serial.println("Vacant. Movement detected.");
+      }
+    } else if (reservation[day-1][period] == 1) {
+      //Notification Module not implemented, therefore tested using Serial output.
+      Serial.println("Reserved.");
+    }
+    Particle.process(); //Process Particle Cloud communication data
+    break;
+  }
+}
