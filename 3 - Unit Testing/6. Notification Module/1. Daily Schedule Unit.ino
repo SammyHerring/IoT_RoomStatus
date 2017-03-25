af5ed05eb0191c90645626ad9d7172c6ba2c3e60
SammyHerring <sammy.herring@me.com>
2017-03-25 23:26:37 +0000

    v0.4.2 r6a
    Unit Test for v0.4.2 regarding the ‘Daily Schedule Notification’ Unit,
    denoted as v0.4.2 r6a.

---
+++ 3 - Unit Testing/6. Notification Module/1. Daily Schedule Unit.ino
@@ -0,0 +1,48 @@
+//Any earlier values are required for optimal operation, but may be discounted from this test.
+
+/* ================ SETUP CODE (RUNS ONCE) ==================== */
+
+void setup() {
+
+}
+
+/* ================ MAIN CODE (RUN REPEATEDLY) ================= */
+
+void loop() {
+  //The main algorithm calls the 'notifyGoodMorning()' function at appropriate intervals.
+  //This will be tested later in the integration tests.
+}
+
+void notifyGoodMorning() {
+  int minSinceMidnight = (Time.local() % 86400) / 60;
+  if ( (periodStart[day-1][0]-15) == minSinceMidnight && !morningNotified ) {
+    Particle.publish("IoTRoomSensor-GoodMorningIntro-G16", notifyData, PRIVATE);
+    delay(3000);
+    for (int i=0;i<6;i++) {
+      notifyString2 = i+1;
+      if ( reservation[day][i] == 1 ) {
+        notifyString4 = "Booked";
+        notifyData = notifyString1+notifyString2+notifyString3+notifyString4;
+        Serial.print("Period Notified - Red ");
+        Serial.println(i);
+        Particle.publish("IoTRoomSensor-GoodMorningBooked-G16", notifyData, PRIVATE);
+      } else if (reservation[day-1][i] == 0) {
+        notifyString4 = "Available";
+        notifyData = notifyString1+notifyString2+notifyString3+notifyString4;
+        Serial.print("Period Notified - Green: ");
+        Serial.println(i);
+        Particle.publish("IoTRoomSensor-GoodMorningAvailable-G16", notifyData, PRIVATE);
+      } else {
+        Serial.print("Error on: ");
+        Serial.println(i);
+      }
+      delay(3000);
+    }
+    Particle.publish("IoTRoomSensor-GoodMorningTS-G16", "", PRIVATE);
+    morningNotified = true;
+    delay(5000);
+    return;
+  } else if (periodEnd[day][5] == minSinceMidnight) {
+    morningNotified = false;
+  }
+}
