4ab7ad44c6847c53de4dcb6a9ecb7e4b6426a7f2
SammyHerring <sammy.herring@me.com>
2017-04-05 02:55:08 +0100

    v0.7.0 r3a
    Unit Test for v0.7.0 regarding the ‘System Setup’ Unit, denoted
    as v0.7.0 r3a.

--- 3 - Unit Testing/3. System Setup Unit.ino
+++ 3 - Unit Testing/3. System Setup Unit.ino
@@ -7,6 +7,10 @@ STARTUP(
 SYSTEM_MODE(MANUAL); //Forces the system to wait for cloud processing until allowed by algorithm.
 SYSTEM_THREAD(ENABLED); //Allows a secondary thread to be used for the cloud processing.
 
+//Setup the parameters of the RGB and OLED functions
+Adafruit_NeoPixel pir_neo = Adafruit_NeoPixel(PIXEL_COUNT_PIR, PIXEL_PIN_PIR, PIXEL_TYPE_PIR);
+Adafruit_SSD1306 display(OLED_RESET);
+
 //The setup and loop components added below to allow the file to successfully compile.
 //However, this unit does not run within them as they are apart of the system setup.
 
